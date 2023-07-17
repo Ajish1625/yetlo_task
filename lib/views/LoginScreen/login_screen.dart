8089b08673b545be7958e6b9148b1bfa221745c5
Ajish1625 <inboxofajish@gmail.com>
2023-07-17 07:13:22 +0530

    task updated

--- lib/views/LoginScreen/login_screen.dart
+++ lib/views/LoginScreen/login_screen.dart
@@ -18,117 +18,122 @@ class _LoginScreenState extends State<LoginScreen> {
     final viewModel = Provider.of<LoginViewModel>(context);
 
     return Scaffold(
-        body: Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Colors.tealAccent,
-            Color(0xFFB71C1C)
-          ], // Update with desired colors
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-        ),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 80),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            const Padding(
-              padding: EdgeInsets.only(
-                top: 50.0,
-              ),
-              child: Image(image: AssetImage('assets/images/unnamed.png')),
-            ),
-            const SizedBox(height: 50),
-            const Text(
-              'Hey Ajish \nLogin now!',
-              style: TextStyle(fontSize: 40),
-            ),
-            const SizedBox(height: 50),
-            Column(
-              children: [
-                Container(
-                  height: MediaQuery.of(context).size.height * 0.05,
-                  width: MediaQuery.of(context).size.width * 0.7,
-                  padding: const EdgeInsets.all(10),
-                  decoration: const BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black45,
-                          blurRadius: 5.0,
-                        )
-                      ]),
-                  child: const TextField(
-                    decoration: InputDecoration(
-                        hintText: "Email or Phone number",
-                        hintStyle:
-                            TextStyle(color: Colors.blueGrey, fontSize: 10),
-                        border: InputBorder.none),
+        body: (viewModel.isLoggedIn)
+            ? const CircularProgressIndicator()
+            : Container(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.tealAccent,
+                      Color(0xFFB71C1C)
+                    ], // Update with desired colors
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
                   ),
                 ),
-                const SizedBox(height: 10),
-                Container(
-                  height: MediaQuery.of(context).size.height * 0.05,
-                  width: MediaQuery.of(context).size.width * 0.7,
-                  padding: const EdgeInsets.all(10),
-                  decoration: const BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black45,
-                          blurRadius: 5.0,
-                        )
-                      ]),
-                  child: const TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                        hintText: "Password",
-                        hintStyle:
-                            TextStyle(color: Colors.blueGrey, fontSize: 10),
-                        border: InputBorder.none),
-                  ),
-                ),
-                const SizedBox(height: 20.0),
-                InkWell(
-                  onTap: () {
-                    viewModel.login(context);
-                  },
-                  child: Container(
-                    height: 40,
-                    width: 120,
-                    decoration: BoxDecoration(
-                      color: Colors.redAccent,
-                      borderRadius: BorderRadius.circular(15.0),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black87,
-                          blurRadius: 4.0,
-                          offset: Offset(0, 2),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 80),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.only(
+                          top: 50.0,
                         ),
-                      ],
-                    ),
-                    child: Center(
-                      child: Text(
-                        'Login',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16.0,
-                        ),
+                        child: Image(
+                            image: AssetImage('assets/images/unnamed.png')),
                       ),
-                    ),
+                      const SizedBox(height: 50),
+                      const Text(
+                        'Hey Ajish \nLogin now!',
+                        style: TextStyle(fontSize: 40),
+                      ),
+                      const SizedBox(height: 50),
+                      Column(
+                        children: [
+                          Container(
+                            height: MediaQuery.of(context).size.height * 0.05,
+                            width: MediaQuery.of(context).size.width * 0.7,
+                            padding: const EdgeInsets.all(10),
+                            decoration: const BoxDecoration(
+                                color: Colors.white,
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20)),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black45,
+                                    blurRadius: 5.0,
+                                  )
+                                ]),
+                            child: const TextField(
+                              decoration: InputDecoration(
+                                  hintText: "Email or Phone number",
+                                  hintStyle: TextStyle(
+                                      color: Colors.blueGrey, fontSize: 10),
+                                  border: InputBorder.none),
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          Container(
+                            height: MediaQuery.of(context).size.height * 0.05,
+                            width: MediaQuery.of(context).size.width * 0.7,
+                            padding: const EdgeInsets.all(10),
+                            decoration: const BoxDecoration(
+                                color: Colors.white,
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20)),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black45,
+                                    blurRadius: 5.0,
+                                  )
+                                ]),
+                            child: const TextField(
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                  hintText: "Password",
+                                  hintStyle: TextStyle(
+                                      color: Colors.blueGrey, fontSize: 10),
+                                  border: InputBorder.none),
+                            ),
+                          ),
+                          const SizedBox(height: 20.0),
+                          InkWell(
+                            onTap: () {
+                              viewModel.login(context);
+                            },
+                            child: Container(
+                              height: 40,
+                              width: 120,
+                              decoration: BoxDecoration(
+                                color: Colors.redAccent,
+                                borderRadius: BorderRadius.circular(15.0),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black87,
+                                    blurRadius: 4.0,
+                                    offset: Offset(0, 2),
+                                  ),
+                                ],
+                              ),
+                              child: Center(
+                                child: Text(
+                                  'Login',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16.0,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          // const CircularProgressIndicator(),
+                        ],
+                      ),
+                    ],
                   ),
                 ),
-                if (viewModel.isLoggedIn) const CircularProgressIndicator(),
-              ],
-            ),
-          ],
-        ),
-      ),
-    ));
+              ));
   }
 }
